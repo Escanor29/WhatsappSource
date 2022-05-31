5490e1362116b79e95ba5b2a227cad22edfc1b7d
Keshav Chandra Ray <keshavray1129@gmail.com>
2022-05-31 18:56:35 +0530

    first commit

---
+++ app/src/main/java/com/example/whatsappsource/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.whatsappsource
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.text.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var number: String = ""
+
+        if(intent.action == Intent.ACTION_PROCESS_TEXT){
+            number = intent.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT).toString()
+        }
+
+        if(number.isDigitsOnly()){
+            startWhatsapp(number)
+        }else{
+            Toast.makeText(this,"Check the no.",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun startWhatsapp(number: String) {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.setPackage("com.whatsapp")
+        // + 91 99 99999999
+        val data: String = if (number[0] == '+') {
+            number.substring(1)
+        } else if (number.length == 10) {
+            "91" + number
+        } else {
+            number
+        }
+        intent.data = Uri.parse("https://wa.me/$data")
+        if (packageManager.resolveActivity(intent, 0) != null){
+            startActivity(intent)
+        }else {
+            Toast.makeText(this, "Please install Whatsapp", Toast.LENGTH_SHORT).show()
+        }
+        finish()
+    }
+}
